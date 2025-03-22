724cf0089c1a9fe3e410477b188190cc02d568b7
YORNSOKHLEN <yornsokhlen33@gmail.com>
2025-03-22 17:34:07 +0700

    new change

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/jsx/elements/Header";
 import Footer from "./components/jsx/elements/Footer";
-import Cart from "./components/jsx/payment/Cart";
+import ScrollToTop from "./components/jsx/elements/ScrollToTop";
 import Home from "./components/jsx/elements/Home";
+import Cart from "./components/jsx/payment/Cart";
 import Shopping from "./components/jsx/payment/shopping";
 import Decade from "./components/jsx/decade/Decade";
 
 const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
