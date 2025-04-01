b20af321e1864e561046870cd7617d42aac37f4b
YORNSOKHLEN <yornsokhlen33@gmail.com>
2025-04-01 15:23:50 +0700

    src/components/jsx/product/GroupProduct.jsx

--- src/components/jsx/product/GroupProduct.jsx
+++ src/components/jsx/product/GroupProduct.jsx
@@ -4,7 +4,7 @@ import DataProduct from "../../js/dataProduct";
 
 const GroupProduct = () => {
   return (
-    <div className=" mx-7 lg:mx-7 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 md:gap-5 lg:gap-10 -z-10 px-10 mt-5 ">
+    <div className=" mx-7 lg:mx-7 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 md:gap-5 lg:gap-10 -z-10 mt-5 ">
       {DataProduct.map((p) => {
         return <Product {...p} key={p.id} />;
       })}
